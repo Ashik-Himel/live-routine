71b67c981a11dc4a99cefca1a6e778d938c3ee74
Ashik-Himel <ashikujjamanhimel@gmail.com>
2022-11-16 11:15:47 +0600

    Batch interface added

--- index.js
+++ index.js
@@ -6,24 +6,32 @@ const routineMain = document.querySelector(".routine-main");
 const nextBtn = document.querySelector(".next-btn");
 const popClose = document.querySelector(".pop-close");
 const popHome = document.querySelector(".pop-home");
+const batch = document.querySelector(".batch");
+const batchMain = document.querySelector(".batch-main");
 
-function navFunc(arg1, arg2, arg3, arg4) {
+function navFunc(arg1, arg2, arg3, arg4, arg5, arg6) {
     arg1.classList.add("active");
     arg2.classList.remove("active");
-    arg3.style.display = "block";
-    arg4.style.display = "none";
+    arg3.classList.remove("active");
+    arg4.style.display = "block";
+    arg5.style.display = "none";
+    arg6.style.display = "none";
 }
 home.addEventListener("click", function() {
     if (home.classList.contains("active")) {}
-    else {navFunc(home, routine, homeMain, routineMain);}
+    else {navFunc(home, routine, batch, homeMain, routineMain, batchMain);}
 })
 routine.addEventListener("click", function() {
     if (routine.classList.contains("active")) {}
-    else {navFunc(routine, home, routineMain, homeMain);}
+    else {navFunc(routine, home, batch, routineMain, homeMain, batchMain);}
+})
+batch.addEventListener("click", function() {
+    if (batch.classList.contains("active")) {}
+    else {navFunc(batch, home, routine, batchMain, homeMain, routineMain);}
 })
 logoText.addEventListener("click", function() {
     if (home.classList.contains("active")) {}
-    else {navFunc(home, routine, homeMain, routineMain);}
+    else {navFunc(home, routine, batch, homeMain, routineMain, batchMain);}
 })
 nextBtn.addEventListener("click", function() {
     popHome.classList.add("active");
